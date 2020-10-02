a8cd20e8e8e9a68bc007ee1a128945970410d83c
jonathan07-1991 <66746329+jonathan07-1991@users.noreply.github.com>
2020-10-02 14:11:21 -0500

    se crea formulario

--- index.php
+++ index.php
@@ -13,17 +13,37 @@
         <hr>
     </header>
 
-    <main>
+    <main class="bg-dark text-white">
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-4">
                     <form action="">
                         
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            <label for="cedula">CEDULA</label>
+                            <input type="number" class="form-control" id="cc">
                         </div>
+                        <div class="form-group">
+                            <label for="nombre">NOMBRE</label>
+                            <input type="text" class="form-control" id="nombre">
+                        </div>
+                        <div class="form-group">
+                            <label for="apellido">APELLIDO</label>
+                            <input type="text" class="form-control" id="apellidos">
+                        </div>
+                        <div class="form-group">
+                            <label for="edad">EDAD</label>
+                            <input type="number" class="form-control" id="edad">
+                        </div>
+                        <div class="form-group">
+                            <label for="telefono">TELEFONO</label>
+                            <input type="number" class="form-control" id="telefono">
+                        </div>
+                        <div class="form-group">
+                            <label for="direccion">DIRECCION</label>
+                            <input type="text" class="form-control" id="direccion">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">REGISTRAR</button>
 
                     </form>
                 </div>
